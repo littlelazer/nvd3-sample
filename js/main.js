b5e45d84d586ab07537f1ff2aafbd52df84bd55e
Eryan Cobham <eryan@cobwebsconsulting.com>
2014-03-06 11:29:36 -0600

    added function for setting up the svg element

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 function Nvd3Graph ( ) {
+  var svg;
+
+  var initialize = function ( ) {
+    setupSvg();
+  };
 
   var generateData = function ( ) {
     var numberOfPoints = 20,
@@ -20,4 +25,35 @@ function Nvd3Graph ( ) {
 
     return graphData;
   };
+
+  var setupSvg = function ( ) {
+    var svgAttributes = {
+      width: 700,
+      height: 300,
+      padding: 25,
+      margin : {
+        top: 5,
+        right: 10,
+        bottom: 5,
+        left: 10
+      }
+    };
+
+    svg = d3.select('#home svg');
+    svg.style({
+      'width': svgAttributes.width + svgAttributes.margin.left + svgAttributes.margin.right,
+      'height': svgAttributes.height + svgAttributes.margin.top + svgAttributes.margin.bottom,
+      'padding': svgAttributes.padding,
+      'margin': '0 auto'
+    });
+    svg.datum( generateData() );
+    svg.transition().duration(500);
+  }
+
+  return {
+    initialize : initialize
+  }
 }
+
+var graph = new Nvd3Graph;
+graph.initialize();
